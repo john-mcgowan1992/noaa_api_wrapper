9b90f4ae59edee13f931fb0540ed765c618bb1ff
John McGowan <mcgowanj92@gmail.com>
2020-11-30 11:13:10 -0500

    Fix queryParam reference, pkg changes

--- server/server.py
+++ server/server.py
@@ -9,7 +9,7 @@ import json
 import os.path
 import requests
 
-from defaults import API_TOKEN
+from server.defaults import API_TOKEN
 
 STATIC_RESOURCES = os.path.abspath("../client/public")
 NOAA_ENDPOINT = "https://www.ncdc.noaa.gov/cdo-web/api/v2/"
@@ -28,7 +28,7 @@ def publicResources(filepath):
 
 @app.route("/api/noaa/data")
 def query_noaa_api():
-    ENDPOINT = NOAA_ENDPOINT + "data?" + request.query_string
+    ENDPOINT = NOAA_ENDPOINT + "data?" + str(request.query_string)
     try:
         api_response = requests.get(ENDPOINT, headers=headers)
         api_response.raise_for_status()
@@ -41,7 +41,7 @@ def query_noaa_api():
 
 @app.route("/api/noaa/dataset/categories")
 def verify_dataset_categories():
-    ENDPOINT = NOAA_ENDPOINT + "datacategories?" + request.query_string
+    ENDPOINT = NOAA_ENDPOINT + "datacategories?" + str(request.query_string)
     try:
         api_response = requests.get(ENDPOINT, headers=headers)
         api_response.raise_for_status()
@@ -51,7 +51,7 @@ def verify_dataset_categories():
 
 @app.route("/api/noaa/dataset/location")
 def verify_dataset_location():
-    ENDPOINT = NOAA_ENDPOINT + "locations?" + request.query_string
+    ENDPOINT = NOAA_ENDPOINT + "locations?" + str(request.query_string)
     try:
         api_response = requests.get(ENDPOINT, headers=headers)
         api_response.raise_for_status()
@@ -72,9 +72,10 @@ def get_location_info():
 
 @app.route("/api/noaa/datatypes")
 def get_datatypes_by_dataset():
-    ENDPOINT = NOAA_ENDPOINT + "datatypes?" + request.query_string
+    ENDPOINT = NOAA_ENDPOINT + "datatypes?"
+    print('REQ: ', request.args)
     try:
-        api_response = requests.get(ENDPOINT, headers=headers)
+        api_response = requests.get(ENDPOINT, headers=headers, params=request.args)
         api_response.raise_for_status()
         return jsonify(api_response.json())
     except requests.exceptions.RequestException as e:
@@ -82,9 +83,9 @@ def get_datatypes_by_dataset():
 
 @app.route("/api/noaa/stations")
 def get_stations_by_coords():
-    ENDPOINT = NOAA_ENDPOINT + "stations?" + request.query_string
+    ENDPOINT = NOAA_ENDPOINT + "stations?"
     try:
-        api_response = requests.get(ENDPOINT, headers=headers)
+        api_response = requests.get(ENDPOINT, headers=headers, params=request.args)
         api_response.raise_for_status()
         return jsonify(api_response.json())
     except requests.exceptions.RequestException as e:
